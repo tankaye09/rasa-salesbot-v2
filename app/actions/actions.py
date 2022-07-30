ffd89f40a76c012d3bf1780e539b5b055f1408e6
Sherene <sherenetan2999@gmail.com>
2022-07-30 20:09:53 +0800

    removed unused function

--- app/actions/actions.py
+++ app/actions/actions.py
@@ -10,7 +10,6 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker, FormValidationAction
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.types import DomainDict
-from rasa_sdk.events import SlotSet
 
 from datetime import datetime
 
